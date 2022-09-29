a7a9392985a0c521c3ea0975495c0f44f4a2cf7b
Hyunin Jeon <kong111312@gmail.com>
2022-09-29 23:05:35 +0900

    ㅎㄹㅇㄹㅇㄴ

--- first.py
+++ first.py
@@ -1,4 +1,8 @@
 from selenium import webdriver as wd
 driver = wd.Chrome(executable_path="chromedriver.exe")
-url = "https://www.naver.com"
-driver.get(url)
+url = "https://www.letskorail.com/ebizprd/prdMain.do"
+driver.get(url) #사이트방문
+
+tabs = driver.window_handles
+print(tabs) #켜진 팝업창 갯수
+
