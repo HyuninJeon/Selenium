fafc6f5b2fd4d51e59fb43338b988b99705f69fc
Hyunin Jeon <kong111312@gmail.com>
2022-10-03 16:11:57 +0900

    검색 예제

--- first.py
+++ first.py
@@ -1,8 +1,18 @@
-from selenium import webdriver as wd
-driver = wd.Chrome(executable_path="chromedriver.exe")
-url = "https://www.letskorail.com/ebizprd/prdMain.do"
+from re import search
+from selenium import webdriver #브라우저에 접근 가능
+from selenium.webdriver.common.keys import Keys #Keys 클래스 == Shift 등을 포함한 키보드 입력
+import time #n초 쉬게 해줌
+
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+url = "https://www.naver.com"
 driver.get(url) #사이트방문
 
-tabs = driver.window_handles
-print(tabs) #켜진 팝업창 갯수
+time.sleep(5)
+
+search = driver.find_element("id", 'query')
+search.send_keys('오늘 날씨')
+search.send_keys(Keys.ENTER)
+time.sleep(2)
+print(driver.current_url)
+driver.close() #탭종료
 
