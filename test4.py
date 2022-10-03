f8ea5f888458eca71c5861ef39bc8ef3a1decfe4
Hyunin Jeon <kong111312@gmail.com>
2022-10-03 19:17:32 +0900

    씨발한글저장안됨

--- test4.py
+++ test4.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd  
 import time 
+# -*- coding:utf-8 -*-
+
 
 finish_line = 10000
 
@@ -71,3 +73,31 @@ for t in elem:
     name = t.find("a", attrs={"class":'yt-simple-endpoint style-scope yt-formatted-string'}).get_text()
     content_url = t.find("a", attrs={"class":'yt-simple-endpoint style-scope ytd-video-renderer'})["href"]
     df.append([name, title , 'https://www.youtube.com/'+content_url])
+
+## 자료 저장
+# 데이터 프레임 만들기
+new = pd.DataFrame(columns=['name', 'title' , 'url_link'])
+
+# 자료 집어넣기
+for i in range(len(df)):
+    new.loc[i] = df[i]
+
+# 저장하기
+# 현재 작업폴더 안의 data 폴더에 저장
+df_dir = "./data/" # 저장할 디렉토리
+new.to_csv(df_dir+"Youtube_search_df.csv", index=False, encoding='UTF-8')
+
+## 컬럼 정보 저장
+# 컬럼 설명 테이블
+col_names = ['name', 'title' ,'url_link']
+col_exp = ['컨텐츠 올린 채널명', '컨텐츠 제목', '연결 링크']
+
+new_exp = pd.DataFrame({'col_names':col_names,
+                        'col_explanation':col_exp})
+
+# 현재 작업폴더 안의 data 폴더에 저장
+df_dir = "./data/" # 저장할 디렉토리
+new_exp.to_csv(df_dir+"Youtube_col_exp.csv", index=False, encoding='UTF-8')
+
+# 브라우저 닫기
+browser.close()
