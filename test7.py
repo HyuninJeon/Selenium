ca0eb23942a9df4702ab4938d99856765ff215c1
Hyunin Jeon <kong111312@gmail.com>
2022-10-10 19:11:35 +0900

    외않되

--- test7.py
+++ test7.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import pandas as pd  
-import time 
 
 path = 'C:\study\Selenium\chromedriver.exe'
 
@@ -25,10 +24,10 @@ time.sleep(5)
 dimen_id = 'heidi.jeon@freedgrouptech.com'
 dimen_pw = 'a5bb0a35e8b6469ca1b707087fa47895'
 
-driver.find_element('class', 'open-modal-button absolute left-2 top-2').click()
+driver.find_element('alt', 'side-menu').click()
 driver.find_element('class','sidebar-signup-login border-solid border border-yellow text-center text-1.5xs text-yellow py-1 px-4').click()
 
-driver.find_element('id', 'input-60').send_keys(dimen_id)
+driver.find_element('id', 'input-61').send_keys(dimen_id)
 
 driver.find_element('id', 'input-62').send_keys(dimen_pw)
 driver.find_element('id', 'input-62').send_keys(Keys.ENTER)
