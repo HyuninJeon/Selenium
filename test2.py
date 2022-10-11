7b69d174bc1978005ab7d21280131f1b6ca48c88
Hyunin Jeon <kong111312@gmail.com>
2022-10-11 22:45:59 +0900

    ㄹㄹ

--- test2.py
+++ test2.py
@@ -3,19 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys  
 import time 
 
-path = 'C:\study\Selenium\chromedriver.exe'
-
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(options=options)
-
-driver.implicitly_wait(3)
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
 url = "https://www.naver.com"
 driver.get(url) #사이트방문
 
 driver.fullscreen_window() #전체화면 f12
 time.sleep(1)
-driver.maximize_window() #꽉찬 화면
+driver.maximize_window() #꽉차 화면
 time.sleep(1)
 driver.set_window_rect(100, 100, 600, 600)
 #특정 좌표(x,y)와 크기(w,h)로 변경
@@ -25,4 +19,4 @@ print(driver.get_window_position)
 
 time.sleep(3) #3초 후 종료
 driver.set_window_position(0,0)
-driver.quit()
+driver.close()
